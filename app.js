4e1d2d71faa2697a3db2a1b632b01cac7611f4d0
mikeDRei <michaelalvesreino@outlook.com>
2022-08-09 15:32:39 -0300

    adding user router

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const app = express();
 const port = 8080;
-// const router = require('./router/router');
+const userRouter = require('./router/user_router');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-// app.use(cors());
-// app.use(router);
+app.use(userRouter);
 
 app.listen(port, function (request, response) {
     console.log(`Servidor rodando na porta ${port}`);
